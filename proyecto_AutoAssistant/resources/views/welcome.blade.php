c5467867ccedce0e4f0e84341a09f1f3493e9457
LuguiM <usis043721@ugb.edu.sv>
2023-04-15 16:33:05 -0600

    intregracion con google

--- proyecto_AutoAssistant/resources/views/welcome.blade.php
+++ proyecto_AutoAssistant/resources/views/welcome.blade.php
@@ -17,23 +17,35 @@
 <body id="body-pd">
     <header class="header" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-        <div class="header_img"> <img src="proyecto_AutoAssistant\public\helpy.jpg" alt=""> </div>
+        <div class="header_img"> <img src="" alt=""> </div>
     </header>
     <div class="l-navbar" id="nav-bar">
+    
         <nav class="nav">
             <div> 
+            
                 <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">AutoAssistant</span> </a>
+                @if (Route::has('login'))
                 <div class="nav_list"> 
-                    <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Menu Principal</span> </a> 
-                    <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> 
-                    <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a>
-                    <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> 
+                
+                @auth
+                    <a href="{{url('/dashboard')}}" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Menu Principal</span> </a> 
+                    @else
+                    <a href="{{ route('login') }}" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> 
+                    <a href="/google-auth/redirect" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a>
+                    
+                    <a href="{{ route('register') }}" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> 
+                    @if (Route::has('register'))
                     <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a>
                     <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> 
+                    @endif
+                    @endauth
                 </div>
+                @endif
             </div> 
                 <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
         </nav>
+        
     </div>
     <!--Container Main start-->
     <div class="height-100 bg-light">
--- proyecto_AutoAssistant/routes/web.php
+++ proyecto_AutoAssistant/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,10 +19,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/registro', function () {
+
+ 
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->stateless()->user();
+ 
+    // $user->token
+    $user = User::updateorCreate([
+        'google_id' => $user_google->id,
+    ],
+    [
+        'name' => $user_google->name,
+        'email' => $user_google->email,
+    ]);
+
+    Auth::login($user);
+
     return view('registro');
 });
 
-Route::get('/login', function () {
-    return view('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
